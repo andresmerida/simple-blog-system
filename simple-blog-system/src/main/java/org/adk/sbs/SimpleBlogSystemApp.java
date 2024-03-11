98d15c85d46ad8fb83224743a2cb0f58ee90e9be
Andres Merida <andresmerida@Andress-MacBook-Pro.local>
2024-03-11 13:03:56 -0400

    initial_commit Initial commit

---
+++ simple-blog-system/src/main/java/org/adk/sbs/SimpleBlogSystemApp.java
@@ -0,0 +1,11 @@
+package org.adk.sbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleBlogSystemApp {
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleBlogSystemApp.class, args);
+    }
+}
